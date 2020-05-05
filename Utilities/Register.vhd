95a396a09ecd4d2709a3e7b9c05186cdd6e1c01c
AhmedEssamDakrory <ahmed.edakrory@gmail.com>
2020-05-05 14:13:58 +0200

    Generic Reg

---
+++ Utilities/Register.vhd
@@ -0,0 +1,28 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
+USE ieee.std_logic_unsigned.ALL;
+ENTITY Reg IS
+ generic (N : Integer := 32);
+ PORT(
+    d   : IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+    Load  : IN STD_LOGIC;
+    clr : IN STD_LOGIC;
+    clk : IN STD_LOGIC;
+    q   : OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0) 
+);
+END Reg;
+ARCHITECTURE arch_register OF Reg IS
+
+BEGIN
+    process(clk, clr)
+    begin
+        if clr = '1' then
+            q <= (others => '0');
+        elsif rising_edge(clk) then
+            if Load = '1' then
+                q <= d;
+            end if;
+        end if;
+    end process;
+END arch_register;
