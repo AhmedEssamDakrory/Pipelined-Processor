43cccffbb01d76a0367335cbb660307799f0fdfa
AhmedEssamDakrory <ahmed.edakrory@gmail.com>
2020-05-09 17:37:52 +0200

    hazrd detection unit must take wb signal

--- Hazard Detection Units/fetch_hazard_detection_unit.vhd
+++ Hazard Detection Units/fetch_hazard_detection_unit.vhd
@@ -8,9 +8,11 @@ dst_branch:in std_logic_vector(2 downto 0);
 dst_dec:in std_logic_vector(2 downto 0);
 src1_dec:in std_logic_vector(2 downto 0);
 swap_dec:in std_logic;
+wb_dec:in std_logic;
 dst_alu:in std_logic_vector(2 downto 0);
 src1_alu: in std_logic_vector(2 downto 0);
 swap_alu: in std_logic;
+wb_alu:in std_logic;
 dst_mem: in std_logic_vector(2 downto 0);
 load_sig_mem:in std_logic;
 flush:out std_logic
@@ -20,9 +22,9 @@ end Fetch_Hazard_Detection_Unit;
 architecture fetch_hazard_arch of Fetch_Hazard_Detection_Unit is
 begin
 
-flush <= '1' when ( (dst_branch = dst_dec) or 
+flush <= '1' when ( (dst_branch = dst_dec and wb_dec = '1') or 
 				  (dst_branch = src1_dec and swap_dec = '1') or 
-				  (dst_branch = dst_alu) or 
+				  (dst_branch = dst_alu and wb_alu = '1') or 
 				  (dst_branch = src1_alu and swap_alu = '1') or
 				  (dst_branch = dst_mem and load_sig_mem = '1')) else
 		 '0';
