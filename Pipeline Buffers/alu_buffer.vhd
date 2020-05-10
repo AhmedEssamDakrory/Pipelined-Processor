cceb9a1e3455fd882e47c705074ba4ec2f00a5e5
AhmedEssamDakrory <ahmed.edakrory@gmail.com>
2020-05-10 15:11:51 +0200

    flags or src signal

--- Pipeline Buffers/alu_buffer.vhd
+++ Pipeline Buffers/alu_buffer.vhd
@@ -9,11 +9,13 @@ ENTITY ALU_Buffer IS
     rst:IN STD_LOGIC;
     
     PcWrBack_in:in std_logic;
-    R_W_Sig_in:in std_logic;
+    Read_Sig_in:in std_logic;
+    Write_sig_in:in std_logic;
     WbSig_in:in std_logic;
     MemToRegSig_in:in std_logic;
     OutPortSig_in:in std_logic;
     SwapSig_in:in std_logic;
+    flagsOrSrc_in:in std_logic;
     DstAddress_in:in std_logic_vector (2 downto 0);
     Src1Address_in:in std_logic_vector (2 downto 0);
     flags_in:in std_logic_vector (2 downto 0);
@@ -21,11 +23,13 @@ ENTITY ALU_Buffer IS
     Rsrc2_in:in std_logic_vector (31 downto 0);
     
     PcWrBack_out:out std_logic;
-    R_W_Sig_out:out std_logic;
+    Read_Sig_out:out std_logic;
+    Write_Sig_out:out std_logic;
     WbSig_out:out std_logic;
     MemToRegSig_out:out std_logic;
     OutPortSig_out:out std_logic;
     SwapSig_out:out std_logic;
+    flagsOrSrc_out:out std_logic;
     DstAddress_out:out std_logic_vector (2 downto 0);
     Src1Address_out:out std_logic_vector (2 downto 0);
     flags_out:out std_logic_vector (2 downto 0);
@@ -40,11 +44,13 @@ BEGIN
     begin
         if rst = '1' then
             PcWrBack_out <= '0' ;
-            R_W_Sig_out <= '0';
+            Read_Sig_out <= '0';
+            Write_Sig_out <= '0';
             WbSig_out <= '0';
             MemToRegSig_out <= '0';
             OutPortSig_out <= '0';
             SwapSig_out <= '0';
+            flagsOrSrc_out <= '0';
             DstAddress_out <= (others => '0');
             Src1Address_out <= (others => '0');
             flags_out <= (others => '0');
@@ -52,12 +58,14 @@ BEGIN
             Rsrc2_out <= (others => '0');
         elsif falling_edge(clk) then
             if Load = '1' then
-                PcWrBack_out <= PcWrBack_in ;
-                R_W_Sig_out <= R_W_Sig_in;
+                PcWrBack_out <= PcWrBack_in;
+                Read_Sig_out <= Read_Sig_in;
+                Write_Sig_out <= Write_Sig_in;
                 WbSig_out <= WbSig_in;
                 MemToRegSig_out <= MemToRegSig_in;
                 OutPortSig_out <= OutPortSig_in;
                 SwapSig_out <= SwapSig_in;
+                flagsOrSrc_out <= flagsOrSrc_in;
                 DstAddress_out <= DstAddress_in;
                 Src1Address_out <= Src1Address_in;
                 flags_out <= flags_in;
