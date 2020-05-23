6b7c885f243659c176803a9f420be738cbf00866
AhmedEssamDakrory <ahmed.edakrory@gmail.com>
2020-05-23 17:25:16 +0200

    fix conflict in branch brediction

--- Processor/Utilities/BranchPredictionBuffer.vhdl
+++ Processor/Utilities/BranchPredictionBuffer.vhdl
@@ -8,11 +8,7 @@ entity BranchPredictionBuffer is
     clk	    	  : in  std_logic;
     Instruction : in  std_logic_vector(4 downto 0);
     BranchPc    : in std_logic_vector(9 downto 0);
-<<<<<<< HEAD
-    input       : in  std_logic;	-- Taken = 1 , Not Taken = 0
-=======
     inp       : in  std_logic;	-- Taken = 1 , Not Taken = 0
->>>>>>> 80fcfca288d5dec9372381e64f70d4f866cdcbed
     we,rst      : in std_logic; -- write 1 , read 0
     oup      : out std_logic
   );
@@ -29,40 +25,10 @@ architecture ArchOfBranchPredictionBuffer of BranchPredictionBuffer is
 
    signal enable :std_logic;
 begin
-<<<<<<< HEAD
-  FSM_Branch_prediction :entity work.FSM(moore) port map(input,clk,rst,we,fsm_input,fsm_output,fsm_signal_output);
-  process(clk,BranchPc,input,PredictionBuffer,fsm_output,temp,rst,we,Instruction,fsm_signal_output)
-  begin
-    if rst='1' then
-        PredictionBuffer<=(others=>"000");
-    else
-        if rising_edge(clk) then
-          if (Instruction(4) and Instruction(3))='1' Then --Branch Instruction
-            if we ='1' then
-              if  PredictionBuffer(to_integer(UNSIGNED(BranchPc)))="000" and input='0' then
-                  PredictionBuffer(to_integer(UNSIGNED(BranchPc)))<="101";
-                  output<=PredictionBuffer(to_integer("000"&UNSIGNED(BranchPc)))(2);
-              else
-                 temp<=PredictionBuffer(to_integer(UNSIGNED(BranchPc)));
-                 fsm_input<=to_Integer(UNSIGNED(temp(1 downto 0)));
-                 PredictionBuffer(to_integer(UNSIGNED(BranchPc)))<=fsm_signal_output&std_logic_vector(to_unsigned(fsm_output,2));
-                 output<=PredictionBuffer(to_integer("000"&UNSIGNED(BranchPc)))(2);
-              end if;
-            else
-              output<= PredictionBuffer(to_integer(UNSIGNED(BranchPc)))(2);
-            end if;
-          else
-            output<='0';
-          end if;
-        end if;
-    end if;
-	end process;
-=======
   FSM_Branch_prediction :entity work.FSM(moore) port map(inp,clk,rst,we,fsm_input,fsm_output,fsm_signal_output);
   enable <= '1' when (Instruction(4) and Instruction(3))='1' else '0';
   temp   <= PredictionBuffer(to_integer(UNSIGNED(BranchPc))) when enable='1' else "ZZZ";
   fsm_input <= to_Integer(UNSIGNED(temp(1 downto 0))) when enable='1' else 0;
   PredictionBuffer(to_integer(UNSIGNED(BranchPc)))<=fsm_signal_output&std_logic_vector(to_unsigned(fsm_output,2)) when enable='1' else "ZZZ";
   oup<=fsm_signal_output when enable='1' else 'Z';
->>>>>>> 80fcfca288d5dec9372381e64f70d4f866cdcbed
 end architecture ArchOfBranchPredictionBuffer;
